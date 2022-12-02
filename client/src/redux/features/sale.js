87cb68c1d2f605e02a76203e78217831e6831d4d
Yann Darwish <yann.darwish@gmail.com>
2022-12-02 15:25:52 +0100

    sale slice

---
+++ client/src/redux/features/sale.js
@@ -0,0 +1,64 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+	id: 0,
+	date: 0,
+	products: [],
+	amount: 0,
+	paymentMethods: {},
+	taxes: {},
+	discount: {},
+	user: "",
+}
+
+const saleSlice = createSlice({
+	name: "sale",
+	initialState,
+	reducers: {
+		resetSale: (state) => {
+			state = initialState
+		},
+		setSaleId: (state, action) => {
+			state.id = action.payload.id
+		},
+		setSaleDate: (state, action) => {
+			state.date = action.payload.date
+		},
+		addProduct: (state, action) => {
+			state.products = state.products.push(action.payload.products)
+		},
+		setSaleAmount: (state, action) => {
+			state.amount = action.payload.amount
+		},
+		setSalePaymentMethods: (state, action) => {
+			state.paymentMethods = action.payload.taxes
+		},
+		setTaxes: (state, action) => {
+			state.taxes = action.payload.taxes
+		},
+		setDiscount: (state, action) => {
+			state.discount = action.payload.discount
+		},
+		setUser: (state, action) => {
+			state.user = action.payload.user
+		},
+		setSale: (state, action) => {
+			state = action.payload
+		},
+	},
+})
+
+export const {
+	resetSale,
+	setSale,
+	setSaleId,
+	setSaleDate,
+	addProduct,
+	setSaleAmount,
+	setSalePaymentMethods,
+	setTaxes,
+	setDiscount,
+	setUser,
+} = saleSlice
+
+export default saleSlice.reducer
--- client/src/redux/reducer/reducer.js
+++ client/src/redux/reducer/reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from "@reduxjs/toolkit"
 import theme from "../features/theme"
 import login from "../features/login"
 import user from "../features/user"
+import sale from "../features/sale"
 import loginApi from "../services/loginApi"
 import productsApi from "../services/productsApi"
 import userApi from "../services/userApi"
@@ -11,6 +12,7 @@ const rootReducer = combineReducers({
 	theme,
 	login,
 	user,
+	sale,
 	[loginApi.reducerPath]: loginApi.reducer,
 	[productsApi.reducerPath]: productsApi.reducer,
 	[userApi.reducerPath]: userApi.reducer,
