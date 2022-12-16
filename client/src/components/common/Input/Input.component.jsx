bb4ff264d54ffdb8a6de363c0d6fc977dba9a6a0
Yann Darwish <yann.darwish@gmail.com>
2022-12-16 13:25:11 +0100

    input props update

--- client/src/components/common/Input/Input.component.jsx
+++ client/src/components/common/Input/Input.component.jsx
@@ -1,13 +1,18 @@
 import { TextField } from "@mui/material"
 
-const Input = ({ label, value, onChange, type, fullWidth}) => {
+const Input = ({ id, label, value, inputAdornment, onChange, type, fullWidth, onClick}) => {
 	return (
 		<TextField
+			id={id}
 			label={label}
 			value={value}
-			onChange={(e) => onChange(e.target.value)}
+			onChange={onChange && ((e) => onChange(e.target.value))}
 			type={type}
 			fullWidth={fullWidth}
+			onClick={onClick}
+			InputProps={
+				inputAdornment && inputAdornment
+			}
 		/>
 	)
 }
