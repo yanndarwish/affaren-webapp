a9703460539fd00ec11c79c2fb6899b85d91bf80
Yann Darwish <yann.darwish@gmail.com>
2023-01-05 16:46:29 +0100

    user performance chart fixchart

--- client/src/components/DASHBOARD/Charts/ChartD/AreaChart.jsx
+++ client/src/components/DASHBOARD/Charts/ChartD/AreaChart.jsx
@@ -3,9 +3,9 @@ import { useEffect, useState } from "react"
 
 const AreaChart = ({ data, theme }) => {
 	const [series, setSeries] = useState([])
-	const [labels, setLabels] = useState([])
+	const labels = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
 	let options = {
-		stroke: { curve: "smooth" },
+		stroke: { width: [5, 5, 4], curve: "straight" },
 		xaxis: {
 			type: "string",
 			categories: labels,
@@ -37,7 +37,7 @@ const AreaChart = ({ data, theme }) => {
 			show: false,
 		},
 		dataLabels: {
-			enabled: false,
+			enabled: true,
 			style: {
 				fontSize: "18px",
 				opacity: 0.4,
@@ -71,33 +71,41 @@ const AreaChart = ({ data, theme }) => {
 	}
 
 	const prepareData = (data) => {
-
 		let series = []
-		let fullLabels = []
 		data?.forEach((user) => {
 
-			let labels = []
+			let localLabels = []
 			let totals = []
 			user.forEach((sale) => {
-
-				if (!labels.includes(sale.sale_day)) {
-					labels.push(sale.sale_day)
-					totals.push(parseFloat(parseFloat(sale.sale_amount).toFixed(2)))
+				if (!localLabels.includes(sale.sale_day)) {
+					localLabels.push(sale.sale_day)
+					// totals.push(parseFloat(parseFloat(sale.sale_amount).toFixed(2)))
+					totals[sale.sale_day - 1] = parseFloat(
+						parseFloat(sale.sale_amount).toFixed(2)
+					)
 				} else {
-					totals[totals.length - 1] = parseFloat((
-						parseFloat(totals[totals.length - 1]) + parseFloat(sale.sale_amount)
-					).toFixed(2))
+					// totals[totals.length - 1] = parseFloat((
+					// 	parseFloat(totals[totals.length - 1]) + parseFloat(sale.sale_amount)
+					// ).toFixed(2))
+					totals[sale.sale_day - 1] = parseFloat(
+						parseFloat(
+							totals[sale.sale_day - 1] + parseFloat(sale.sale_amount)
+						).toFixed(2)
+					)
+				}
+			})
+			labels.forEach(day => {
+				if (totals[day-1] === undefined) {
+					totals[day -1] = null
 				}
 			})
 			let serie = { name: user[0]?.sale_user, data: totals }
 			series.push(serie)
             // Check here to update labels for multiple users
-			fullLabels = labels
 		})
-		setLabels(fullLabels)
+		// setLabels(fullLabels)
         setSeries(series)
 	}
-    console.log(labels)
 
 	useEffect(() => {
 		prepareData(data)
