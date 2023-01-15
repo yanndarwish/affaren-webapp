53b0483a61ed5efbd9056e86650b457b171bfbae
Yann Darwish <yann.darwish@gmail.com>
2023-01-15 16:38:37 +0100

    api IP update

--- client/src/redux/services/loginApi.js
+++ client/src/redux/services/loginApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const loginApi = createApi({
 	reducerPath: "loginApi",
 	baseQuery: fetchBaseQuery({
-		baseUrl: "http://localhost:4001/",
+		baseUrl: "http://192.168.1.41:4001/",
 	}),
 	tagTypes: ["Sales"],
 	endpoints: (builder) => ({
