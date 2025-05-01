08cc94ad802187cf7c3865591494aceb02c3544a
Yann Darwish <yann.darwish@gmail.com>
2025-05-01 12:48:22 +0200

    fix: sale taxes

--- client/src/lib/pos/index.js
+++ client/src/lib/pos/index.js
@@ -5,79 +5,58 @@ export const getAmount = (products) => {
 	return amount.toFixed(2)
 }
 
+export const roundToTwo = (number) => {
+	return Math.round((number + Number.EPSILON) * 100) / 100
+}
+
+const calculateTaxes = (details, product, taxRate) => {
+	const totalProductPrice = roundToTwo(product.price * product.quantity)
+	const taxRateMultiplier = 1 + taxRate / 100
+	const ht = roundToTwo(totalProductPrice / taxRateMultiplier)
+	const tva = roundToTwo(totalProductPrice - ht)
+
+	const taxKey = `tva${getTaxIndex(taxRate)}`
+	const htKey = `ht${getTaxIndex(taxRate)}`
+	const totalKey = `total${getTaxIndex(taxRate)}`
+
+	return {
+		...details,
+		[taxKey]: details[taxKey]
+			? roundToTwo(details[taxKey] + tva)
+			: roundToTwo(tva),
+		[htKey]: details[htKey] ? roundToTwo(details[htKey] + ht) : roundToTwo(ht),
+		[totalKey]: details[totalKey]
+			? roundToTwo(details[totalKey] + totalProductPrice)
+			: roundToTwo(totalProductPrice),
+		totalTva: details.totalTva
+			? roundToTwo(details.totalTva + tva)
+			: roundToTwo(tva),
+		totalHt: details.totalHt
+			? roundToTwo(details.totalHt + ht)
+			: roundToTwo(ht),
+	}
+}
+
+const getTaxIndex = (taxRate) => {
+	switch (taxRate) {
+		case 5.5:
+			return 1
+		case 2.1:
+			return 2
+		case 20:
+			return 3
+		default:
+			return 1
+	}
+}
+
 export const updateTaxes = (products) => {
 	let taxesDetails = {}
+
 	products.forEach((product) => {
-		let ht = (product.price / (1 + parseFloat(product.taxe) / 100)).toFixed(2)
-		let tva = (product.price - ht).toFixed(2)
-		switch (parseFloat(product.taxe)) {
-			case 5.5:
-				taxesDetails = {
-					...taxesDetails,
-					tva1: taxesDetails.tva1
-						? (parseFloat(taxesDetails.tva1) + parseFloat(tva)).toFixed(2)
-						: tva,
-					ht1: taxesDetails.ht1
-						? (parseFloat(taxesDetails.ht1) + parseFloat(ht)).toFixed(2)
-						: ht,
-					total1: taxesDetails.total1
-						? (
-								parseFloat(taxesDetails.total1) + parseFloat(product.price)
-						  ).toFixed(2)
-						: product.price,
-					totalTva: taxesDetails.totalTva
-						? (parseFloat(taxesDetails.totalTva) + parseFloat(tva)).toFixed(2)
-						: tva,
-					totalHt: taxesDetails.totalHt
-						? (parseFloat(taxesDetails.totalHt) + parseFloat(ht)).toFixed(2)
-						: ht,
-				}
-				break
-			case 2.1:
-				taxesDetails = {
-					...taxesDetails,
-					tva2: taxesDetails.tva2
-						? (parseFloat(taxesDetails.tva2) + parseFloat(tva)).toFixed(2)
-						: tva,
-					ht2: taxesDetails.ht2
-						? (parseFloat(taxesDetails.ht2) + parseFloat(ht)).toFixed(2)
-						: ht,
-					total2: taxesDetails.total2
-						? (
-								parseFloat(taxesDetails.total2) + parseFloat(product.price)
-						  ).toFixed(2)
-						: product.price,
-					totalTva: taxesDetails.totalTva
-						? (parseFloat(taxesDetails.totalTva) + parseFloat(tva)).toFixed(2)
-						: tva,
-					totalHt: taxesDetails.totalHt
-						? (parseFloat(taxesDetails.totalHt) + parseFloat(ht)).toFixed(2)
-						: ht,
-				}
-				break
-			case 20:
-				taxesDetails = {
-					...taxesDetails,
-					tva3: taxesDetails.tva3
-						? (parseFloat(taxesDetails.tva3) + parseFloat(tva)).toFixed(2)
-						: tva,
-					ht3: taxesDetails.ht3
-						? (parseFloat(taxesDetails.ht3) + parseFloat(ht)).toFixed(2)
-						: ht,
-					total3: taxesDetails.total3
-						? (
-								parseFloat(taxesDetails.total3) + parseFloat(product.price)
-						  ).toFixed(2)
-						: product.price,
-					totalTva: taxesDetails.totalTva
-						? (parseFloat(taxesDetails.totalTva) + parseFloat(tva)).toFixed(2)
-						: tva,
-					totalHt: taxesDetails.totalHt
-						? (parseFloat(taxesDetails.totalHt) + parseFloat(ht)).toFixed(2)
-						: ht,
-				}
-				break
-			default:
+		const taxRate = parseFloat(product.taxe)
+		if (taxRate === 5.5 || taxRate === 2.1 || taxRate === 20) {
+			taxesDetails = calculateTaxes(taxesDetails, product, taxRate)
 		}
 	})
 
@@ -97,16 +76,16 @@ export const getTotalOriginalPrice = (sale) => {
 	// Sum up original prices from the discount array
 	const sumOfDiscountedPrices = sale.discount.reduce((acc, product) => {
 		// find quantity of product in sale.products
-		const quantity = sale.products.find(
-			(p) => p.id === product.productId
-		)?.quantity || 0
+		const quantity =
+			sale.products.find((p) => p.id === product.productId)?.quantity || 0
 		return acc + product.originalPrice * quantity
 	}, 0)
 
 	// Sum up prices of products that are NOT in the discount array
 	const sumOfOriginalPrices = sale.products.reduce((acc, product) => {
 		if (!discountedProductIds.has(product.id)) {
-			const quantity = sale.products.find((p) => p.id === product.id)?.quantity || 0
+			const quantity =
+				sale.products.find((p) => p.id === product.id)?.quantity || 0
 			return acc + product.price * quantity
 		}
 		return acc
@@ -118,9 +97,8 @@ export const getTotalOriginalPrice = (sale) => {
 export const getTotalReduction = (sale) => {
 	return roundUpToTwoDecimals(
 		sale.discount.reduce((acc, product) => {
-			const quantity = sale.products.find(
-				(p) => p.id === product.productId
-			)?.quantity || 0
+			const quantity =
+				sale.products.find((p) => p.id === product.productId)?.quantity || 0
 			return acc + product.reduction * quantity
 		}, 0)
 	)
